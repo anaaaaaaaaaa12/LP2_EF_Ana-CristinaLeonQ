623e319b0d4529367757bd12a835768626eddaa7
anaaaaaaaaaa12 <130624365+anaaaaaaaaaa12@users.noreply.github.com>
2024-07-07 13:01:47 -0500

    Add files via upload

---
+++ Script LP2_EF_AnnaLeon.sql
@@ -0,0 +1,7 @@
+create database bd_annaleon;
+
+SELECT * FROM bd_annaleon.usuarios;
+SELECT * FROM bd_annaleon.categorias;
+SELECT * FROM bd_annaleon.productos;
+
+insert into bd_annaleon.categorias(nombre) values ("LACTEOS"), ("BEBIDAS"), ("CEREALES"), ("GALLETAS");
